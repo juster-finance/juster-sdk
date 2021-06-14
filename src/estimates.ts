a08813b769d8ae1ee2c2ccdb2a5aedfe5ca0023e
ztepler <ztepler@gmail.com>
2021-06-14 17:17:21 +0300

    added estimateShares func and test

--- src/estimates.ts
+++ src/estimates.ts
@@ -24,3 +24,29 @@ export function estimateBetReward(
 
   return valueBN.plus(winDeelta);
 }
+
+/**
+ * Estimated the amount of the new liquidity shares that would be given to provider
+ *
+ * @param event Baking Bet event where the provide liquidity goes to
+ * @param provided new liquidity amount
+ * @returns calculated possible shares count for this event state if new provider
+ *  adds "provided"
+ */
+ export function estimateShares(
+  event: any,
+  provided: BigNumber.Value
+): BigNumber {
+  const investedBN = new BigNumber(provided);
+  const totalShares = new BigNumber(event.totalLiquidityShares);
+
+  // TODO: if first LP then just return precision
+  // or raise error if there are no totalShares
+  // TODO: test this case with event.totalLiquidityShares === 0
+
+  const maxPool = BigNumber.maximum(event.poolAboveEq, event.poolBellow);
+  const newShares = totalShares.times(provided).idiv(maxPool);
+
+  return newShares;
+}
+
--- test/estimates.test.ts
+++ test/estimates.test.ts
@@ -1,27 +1,38 @@
-import { estimateBetReward } from "../src/estimates";
+import { format } from "path/posix";
+import { estimateBetReward, estimateShares } from "../src/estimates";
+import events from "./data/events.json"
+import BigNumber from "bignumber.js";
+
 
 test("estimateBetReward", async () => {
-  const betValue = 1_000_000;
+  let reward: string;
 
-  let event = {
-    poolAboveEq: 1_000_000,
-    poolBellow: 1_000_000
-  };
-
-  let reward = estimateBetReward(event, "AboveEq", 1_000_000).toFixed();
+  reward = estimateBetReward(events["1m:1m"], "AboveEq", 1_000_000).toFixed();
   expect(reward).toBe("1500000");
 
-  reward = estimateBetReward(event, "Bellow", 1_000_000).toFixed();
+  reward = estimateBetReward(events["1m:1m"], "Bellow", 1_000_000).toFixed();
   expect(reward).toBe("1500000");
 
-  event = {
-    poolAboveEq: 1_000,
-    poolBellow: 4_000
-  };
-
-  reward = estimateBetReward(event, "AboveEq", 1_000).toFixed();
+  reward = estimateBetReward(events["1k:4k"], "AboveEq", 1_000).toFixed();
   expect(reward).toBe("3000");
 
-  reward = estimateBetReward(event, "Bellow", 6_000).toFixed();
+  reward = estimateBetReward(events["1k:4k"], "Bellow", 6_000).toFixed();
   expect(reward).toBe("6600");
+});
+
+
+test("estimateShares", async () => {
+  let shares: string;
+
+  shares = estimateShares(events["1m:1m"], 1_000_000).toFixed();
+  expect(shares).toBe("1000000");
+
+  shares = estimateShares(events["1m:1m"], 5_000_000).toFixed();
+  expect(shares).toBe("5000000");
+
+  shares = estimateShares(events["1k:4k"], 2_000).toFixed();
+  expect(shares).toBe("500");
+
+  shares = estimateShares(events["1k:4k"], 10_000).toFixed();
+  expect(shares).toBe("2500");
 });
