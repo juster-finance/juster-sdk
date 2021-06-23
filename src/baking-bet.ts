698ace35d5024e953ed7a38881f2a0511e065346
ztepler <ztepler@gmail.com>
2021-06-23 16:52:55 +0300

    creating tezos instance in baking bet object, started to implement bet & provideLiquidity methods

--- src/baking-bet.ts
+++ src/baking-bet.ts
@@ -1,9 +1,111 @@
-export class BakingBet {
-  private __network: "mainnet" | "testnet";
+// import { ContractMethod } from "./types";
+import { ContractMethod, Wallet, TezosToolkit } from "@taquito/taquito";
+import { ParameterSchema } from "@taquito/michelson-encoder";
+import { BeaconWallet } from '@taquito/beacon-wallet';
+import config from "./config.json"
+import BigNumber from "bignumber.js";
+import { NetworkType } from '@airgap/beacon-sdk';
 
-  constructor(network: "mainnet" | "testnet") {
-    this.__network = network;
-    console.log("BakingBet creation")
-  }
+
+type Network = "mainnet" | "testnet";
+type EntrypointName = "bet" | "provideLiquidity" | "withdraw";
+type BetType = "aboveEq" | "below";
+
+// TODO: move all precisions in config into one objkt?
+
+export class BakingBet {
+  protected _network: Network;
+  protected _tezos: TezosToolkit;
+  protected _provider: BeaconWallet;
+  protected _contractAddress: string;
+  protected _entrypoints: Map<string, ParameterSchema>;
+
+  constructor(
+    network: Network,
+    contractAddress: string,
+    tezos: TezosToolkit,
+    entrypoints: Record<string, any>,
+    appName: string
+  ) {
+    this._network = network;
+    this._tezos = tezos;
+
+    // TODO: is it possible to get wallet provider using tezos instance?
+    // I did not find it, so I am saving this provider to call requestPermissions
+    this._provider = new BeaconWallet({
+      name: appName,
+      preferredNetwork: network === "mainnet"
+        ? NetworkType.MAINNET
+        : NetworkType.FLORENCENET
+    });
+
+    this._tezos.setWalletProvider(this._provider);
+    this._contractAddress = contractAddress;
+    this._entrypoints = new Map<string, ParameterSchema>(
+      Object.entries(entrypoints).map(([name, typeExpr]) => {
+        return [name, new ParameterSchema(typeExpr)];
+      }),
+    );
+  };
+
+  static create(
+    network: Network,
+  ) {
+    const networkSettings = config.networks[network];
+    const { contractAddress, rpcNode } = networkSettings;
+    const { appName, entrypoints } = config;
+
+    const tezos = new TezosToolkit(rpcNode);
+    return new BakingBet(
+      network,
+      contractAddress,
+      tezos,
+      entrypoints,
+      appName
+    )
+  };
+
+  sync() {
+    // Calls request permissions:
+    return this._provider.requestPermissions({
+      network: {type: NetworkType.FLORENCENET}
+    });
+  };
+
+  makeContractMethod(
+    entrypoint: EntrypointName,
+    args: any
+  ) {
+    return new ContractMethod(
+      this._tezos.wallet,
+      this._contractAddress,
+      this._entrypoints.get(entrypoint)!,
+      entrypoint,
+      args);
+  };
+
+  bet(
+    // TODO: make interface for each entrypoint (?)
+    eventId: number,
+    bet: BetType,
+    betValue: BigNumber.Value,
+    minimalWinAmount: BigNumber.Value,
+  ) {
+    const args = [bet, 'unit', eventId, minimalWinAmount];
+    // TODO: need to add betValue to amount somehow
+    return this.makeContractMethod("bet", args);
+  };
+
+  provideLiquidity(
+    eventId: number,
+    expectedRatioAboveEq: BigNumber.Value,
+    expectedRatioBellow: BigNumber.Value,
+    maxSlippage: BigNumber.Value,
+  ) {
+    const args = [eventId, expectedRatioAboveEq, expectedRatioBellow, maxSlippage];
+    return this.makeContractMethod("provideLiquidity", args);
+  };
+
+  // TODO: withdraw call
 }
 
--- test/baking-bet.test.ts
+++ test/baking-bet.test.ts
@@ -1,6 +1,9 @@
 import { BakingBet } from "../src/baking-bet";
 
-test("testingTest", async () => {
-  const bb = new BakingBet("testnet");
+test("test baking bet creation", async () => {
+  /*
+  // Next line is failing with: 
+  // The promise rejected with the reason "ReferenceError: localStorage is not defined".
+  const bb = BakingBet.create("testnet");
+  */
 });
-
