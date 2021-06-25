091b7d318051a2422135355d639595ea8f5d0b06
ztepler <ztepler@gmail.com>
2021-06-25 19:09:32 +0300

    sdk-ui-demo: added provideLiquidity slippage

--- projects/sdk-ui-demo/src/components/ProvideLiquidityForm.tsx
+++ projects/sdk-ui-demo/src/components/ProvideLiquidityForm.tsx
@@ -19,22 +19,43 @@ export const ProvideLiquidityForm: FunctionComponent<ProvideLiquidityProps> = ({
   const [amount, setAmount] = useState<BigNumber>(new BigNumber(0));
   const [newShares, setNewShares] = useState<BigNumber>(new BigNumber(0));
   const [maxSlippage, setMaxSlippage] = useState<BigNumber>(new BigNumber(0.05));
-  const [ratio, setRatio] = useState<BigNumber>(new BigNumber(0.05));
+  const [ratio, setRatio] = useState<BigNumber>(new BigNumber(0));
+
+  // MinRatio & MaxRatio is ratios including slippage that can
+  // happen during transaction processing (one for + and one for -):
+  const [minRatio, setMinRatio] = useState<BigNumber>(new BigNumber(0));
+  const [maxRatio, setMaxRatio] = useState<BigNumber>(new BigNumber(0));
 
   if (event === null) {
     return <div></div>
   };
 
-  const handleAmountChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    const provided = new BigNumber(e.target.value);
-    setNewShares(estimateShares(event, provided));
-
+  const updateValues = (
+    amount: BigNumber,
+    slippage: BigNumber,
+  ) => {
+    setNewShares(estimateShares(event, amount));
+    setAmount(amount);
     // Ratio for shares calculated for one pool, let it be aboveEq:
-    setRatio(calculateRatio(event, "aboveEq"));
-    setAmount(provided);
+    const ratio = calculateRatio(event, "aboveEq");
+    setRatio(ratio);
+    setMaxSlippage(slippage);
+    const slippageMultiplier = slippage.plus(new BigNumber(1));
+    setMinRatio(ratio.div(slippageMultiplier));
+    setMaxRatio(ratio.times(slippageMultiplier));
   };
 
-  // TODO: need to show new shares that would receive participant
+  const handleAmountChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const providedAmount = new BigNumber(e.target.value);
+    updateValues(providedAmount, maxSlippage);
+  };
+
+  const handleMaxSluppageChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    const newSlippage = new BigNumber(e.target.value);
+    updateValues(amount, newSlippage);
+  };
+
+  // TODO: update all values after initialization?
   // TODO: add slippage percent here? how to show it? maybe show min/max ratio?
   const handleProvideLiquidity = async (e: FormEvent<HTMLButtonElement>) => {
 
@@ -63,12 +84,22 @@ export const ProvideLiquidityForm: FunctionComponent<ProvideLiquidityProps> = ({
         <span>Amount:</span>
         <input
           onChange={handleAmountChange}
-          defaultValue={amount.toFixed() || ''}
+          defaultValue={amount.toFixed(6) || ''}
         />
       </p>
       <p>
-        <span>Ratio:</span>
-        <span>{ratio.toFixed(3)}</span>
+        <span>Max slippage:</span>
+        <input
+          onChange={handleMaxSluppageChange}
+          defaultValue={maxSlippage.toFixed(3) || ''}
+        />
+      </p>
+      <p>
+        <span>AboveEq Ratio:</span>
+        <span>{ratio.toFixed(3)}
+          (min: {minRatio.toFixed(3)},
+          max: {maxRatio.toFixed(3)}
+        )</span>
       </p>
       <p>
         <span>New shares:</span>
