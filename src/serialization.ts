2fc1040202301e6d84fae96360092f501d8dba92
ztepler <ztepler@gmail.com>
2021-06-30 21:00:50 +0300

    added empty event to deserialization process

--- src/serialization.ts
+++ src/serialization.ts
@@ -5,14 +5,29 @@ import {
 import BigNumber from "bignumber.js";
 
 
+const emptyEvent: EventType = {
+  poolAboveEq: new BigNumber(0),
+  poolBelow: new BigNumber(0),
+  totalLiquidityShares: new BigNumber(0),
+  createdTime: new Date(0),
+  betsCloseTime: new Date(0),
+  liquidityPercent: new BigNumber(0)
+};
+
 export const deserializeEvent = (rawEvent: any): EventType => {
+  // TODO: where should happen this check that event is empty?
+  if ((rawEvent === undefined) || (rawEvent === null)) {
+    // TODO: maybe it is good to raise some error here?
+    return emptyEvent
+  };
+
   return {
     poolAboveEq: new BigNumber(rawEvent.pool_above_eq),
     poolBelow: new BigNumber(rawEvent.pool_below),
     totalLiquidityShares: new BigNumber(rawEvent.total_liquidity_shares),
     createdTime: new Date(rawEvent.created_time),
     betsCloseTime: new Date(rawEvent.bets_close_time),
-    liquidityPercent: new BigNumber(rawEvent.liquidity_percent)        
+    liquidityPercent: new BigNumber(rawEvent.liquidity_percent)
   };
 };
 
@@ -26,7 +41,8 @@ const emptyPosition: PositionType = {
 
 export const deserializePosition = (rawPosition: any): PositionType => {
   // TODO: where should happen this check that position is empty?
-  if (rawPosition === undefined) {
+  if ((rawPosition === undefined) || (rawPosition === null)) {
+    // TODO: maybe it is good to raise some error here?
     return emptyPosition
   };
 
