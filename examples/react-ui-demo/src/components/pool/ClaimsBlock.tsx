a2693ff0b96fc198c3f152fd0f7a712fcfc400f5
ztepler <ztepler@gmail.com>
2022-08-09 01:05:30 +0300

    added withdraw all button

--- examples/react-ui-demo/src/components/pool/ClaimsBlock.tsx
+++ examples/react-ui-demo/src/components/pool/ClaimsBlock.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, FormEvent } from 'react';
 import { WithdrawClaimButton } from './WithdrawClaimButton'
+import { processOperationSucceed, processOperationError } from '../../utility'
 import {
   JusterPool,
   ClaimsType
@@ -14,41 +15,51 @@ export type ClaimsBlockProps = {
 
 export const ClaimsBlock: FunctionComponent<ClaimsBlockProps> = ({ pkh, justerPool, claims }) => {
 
+  const handleWithdrawAll = async (e: FormEvent<HTMLButtonElement>) => {
+    // TODO: limit claims to 30
+    justerPool.withdrawLiquidity(claims)
+      .then(processOperationSucceed)
+      .catch(processOperationError);
+  };
+
   return (
     <div className="Grid">
       <h3>Claims:</h3>
       {claims.length > 0 &&
-        <table className="Table">
-          <thead>
-            <tr>
-              <th>position id</th>
-              <th>event id</th>
-              <th>amount</th>
-              <th>is withdrawn</th>
-              <th>action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {claims.map(claim => {
-                  return (
-                    <tr key={claim.id}>
-                      <td>{claim.positionId}</td>
-                      <td>{claim.eventId}</td>
-                      <td>{claim.amount.toFixed(6)} xtz</td>
-                      <td>{claim.withdrawn ? "yay" : "not yet"}</td>
-                      <td>
-                        <WithdrawClaimButton
-                          claim={claim}
-                          justerPool={justerPool}
-                        />
-                      </td>
-                    </tr>
-                  )
-                }
-              )
-            }
-          </tbody>
-        </table>
+        <div>
+          <table className="Table">
+            <thead>
+              <tr>
+                <th>position id</th>
+                <th>event id</th>
+                <th>amount</th>
+                <th>is withdrawn</th>
+                <th>action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {claims.map(claim => {
+                    return (
+                      <tr key={claim.id}>
+                        <td>{claim.positionId}</td>
+                        <td>{claim.eventId}</td>
+                        <td>{claim.amount.toFixed(6)} xtz</td>
+                        <td>{claim.withdrawn ? "yay" : "not yet"}</td>
+                        <td>
+                          <WithdrawClaimButton
+                            claim={claim}
+                            justerPool={justerPool}
+                          />
+                        </td>
+                      </tr>
+                    )
+                  }
+                )
+              }
+            </tbody>
+          </table>
+          <button onClick={handleWithdrawAll}>withdraw all</button>
+        </div>
       }
     </div>
   );
